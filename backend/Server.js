e7428dc0abee6eef94e82f27454f86d5040a33ae
JalilahAhmad <jalilahfallatah@gmail.com>
2021-12-25 02:09:10 +0300

    create server with database

--- backend/Server.js
+++ backend/Server.js
@@ -4,11 +4,38 @@ const db=require("./DB");
 const Todo=require("./todo");
 console.log(Todo);
 
+app.use(Express.json())
+
 app.get('/',(req,res)=>{
 
     res.json('Get / is workin')
 })
 
-app.listen(500,()=>{
+app.get('/tasks',(req,res)=>{
+    Todo.find({},(err,data)=>{
+        if(err){ 
+            console.log("Error")
+        }else{
+            res.json(data)
+        }       
+    })
+})
+
+
+
+app.post('/tasks',(req,res)=>{
+    Todo.create(req.body,(err,newTask)=>{
+        if(err){ 
+            console.log("Error")
+        }else{
+         
+            res.status(201).json("create new Todo successfully")
+        }
+        
+    })
+})
+
+
+app.listen(5000,()=>{
     console.log('Server is workin')
 })
